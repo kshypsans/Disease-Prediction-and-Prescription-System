22aa4e80c4890c8d2a9ae8e8813f48cbc0ebaf5c
SANS <146074288+kshypsans@users.noreply.github.com>
2025-07-12 17:28:10 +0530

    Add files via upload

---
+++ Frontend/app/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src="/logo.png" alt="Disease Predictor Logo" />
+      </div>
+      <div className="tagline">
+        <h2>HealthBot</h2>
+      </div>
+    </nav>
+  );
+}
+
+
